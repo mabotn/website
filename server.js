cc33dd098dabb4cd682e5cd055a3e1d99ad26619
Mohamed Anas <mabo.tn@outlook.com>
2016-07-26 23:11:29 +0100

    Updating port from 1337 to 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 
 // set the port for the web app (process.env.PROT for production)
-var port = process.env.PORT || 1337
+var port = process.env.PORT || 3000
 
 // use public directory for website files (html, css, js ...)
 app.use(express.static(__dirname + '/public'));
