c8f8688c65d9ff53b1f78947645a0fd273000bd2
Mohamed Anas <mabo.tn@outlook.com>
2016-07-26 22:34:42 +0100

    Adding public directory for website assets

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+// load express.js module into app
 var express = require('express')
 var app = express()
 
+// set the port for the web app (process.env.PROT for production)
 var port = process.env.PORT || 1337
 
+// use public directory for website files (html, css, js ...)
+app.use(express.static(__dirname + '/public'));
+
+// to return index.html 
 app.get('/', function(req, res) {
-    res.send('Hi, I\'m Mabo')
+    res.sendFile('index.html')
 })
 
+// run the NodeJs application
 app.listen(port)
