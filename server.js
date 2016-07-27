8da79635d61004e2434a418a3d639f511251737a
Mohamed Anas <mabo.tn@outlook.com>
2016-07-27 10:56:01 +0100

    Enabling SSL from NodeJS instead of Heroku Plugin

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-// load express.js module into app
+// load node-js modules
 var express = require('express')
+var https = require('https')
+var http = require('http')
+var fs = require('fs')
+
+// create express.js server instance
 var app = express()
+
+// https SSL options
+var httpsOptions = {
+    cert: fs.readFileSync('./ssl/cert.pem'),
+    key : fs.readFileSync('./ssl/key.pem')
+}
 
 // set the port for the web app (process.env.PROT for production)
 var port = process.env.PORT || 3000
@@ -13,19 +24,16 @@ app.get('/', function(req, res) {
     res.sendFile('index.html')
 })
 
-// Redirect non-www to www with HTTPS
-app.all(/.*/, function(req, res, next) {
-    var host = req.header("host")
-    if (host.match(/^www\..*/i)) {
-        next()
-    } else {
-        res.redirect(301, "https://www." + host)
-    }
-});
-
+// Let's Encrypt verification key for SSL certification
 app.get('/.well-known/acme-challenge/KdTkjttPdMDHQuHzpj1JBjudKFLn58C07-W87SalmEY', function(req, res) {
     res.send('KdTkjttPdMDHQuHzpj1JBjudKFLn58C07-W87SalmEY.k1vqNLUTVmPEyRVc5XqD4rePpeD6vEYAtwZNrz4g8Ao')
 })
 
-// run the NodeJs application
-app.listen(port)
+// run the web server
+if (port == 3000) {
+    // if in development mode
+    app.listen(port)
+} else {
+    // if in production mode
+    https.createServer(httpsOptions, app).listen(port)
+}
