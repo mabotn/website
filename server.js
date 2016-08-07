9384589c5fb9a482c94665389f66931a2028e346
Mohamed Anas <mabo.tn@outlook.com>
2016-08-07 14:35:25 +0100

    Adding internal quotes API

--- server.js
+++ server.js
@@ -8,11 +8,25 @@ var app = express()
 var port = process.env.PORT || 3000
 
 // use public directory for website files (html, css, js ...)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 // to return index.html
 app.get('/', function(req, res) {
     res.sendFile('index.html')
+})
+
+// API for quotes
+app.get('/api/v1/quotes', function(req, res) {
+    // require fs nodule
+    var fs = require('fs')
+    // read quotes/quotes.json file
+    var content = fs.readFileSync('quotes/quotes.json')
+    // parse JSON into JS Object
+    var jsonContent = JSON.parse(content)
+    // get a random index
+    var random = Math.floor(Math.random() * jsonContent.length)
+    // return json response of a random quote
+    res.json(jsonContent[random])
 })
 
 // Let's Encrypt verification key for SSL certification
